39b8583350327c9f7f2f2a18bcce6e779c271b89
Andrew <hopisaurus@gmail.com>
2022-11-10 17:25:40 -0500

    extend to backpack

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -2,8 +2,8 @@ import { useMemo } from "react";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter} from "@solana/wallet-adapter-wallets";
-import { ConnectionProvider, WalletProvider, useWallet } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter, BackpackWalletAdapter} from "@solana/wallet-adapter-wallets";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 
 import "../styles/App.css";
 import "../styles/globals.css";
@@ -13,14 +13,13 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 const App = ({ Component, pageProps }) => {
     const network = WalletAdapterNetwork.Devnet;
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
-    const wallet = useWallet(() => [wallets], [network]);
+    const wallets = useMemo(() => [new PhantomWalletAdapter(), new BackpackWalletAdapter], [network]);
+
     return (
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={wallets}  autoConnect>
                 <WalletModalProvider>
                     <Component {...pageProps} />
-                    {console.log("is wallet connected? "+ wallet.connected)}
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
