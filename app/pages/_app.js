91e438277ae3e4cc0894948d53e218bde182a3a2
Andrew <hopisaurus@gmail.com>
2022-11-09 23:01:59 -0500

    mods for phase1

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -2,8 +2,8 @@ import { useMemo } from "react";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter} from "@solana/wallet-adapter-wallets";
+import { ConnectionProvider, WalletProvider, useWallet } from "@solana/wallet-adapter-react";
 
 import "../styles/App.css";
 import "../styles/globals.css";
@@ -14,16 +14,18 @@ const App = ({ Component, pageProps }) => {
     const network = WalletAdapterNetwork.Devnet;
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
     const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
-
+    const wallet = useWallet(() => [wallets], [network]);
     return (
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
+            <WalletProvider wallets={wallets}  autoConnect>
                 <WalletModalProvider>
                     <Component {...pageProps} />
+                    {console.log("is wallet connected? "+ wallet.connected)}
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
     );
+
 };
 
 export default App;
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,20 +1,40 @@
 import React from "react";
-
+import dynamic from 'next/dynamic';
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE= "hopisaurus";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_LOGO = "twitter-logo.svg";
+
+const WalletMultiButton = dynamic(
+    async () => 
+        (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+        { ssr: false }
+    );
 
 const Home = () => {
+    // actions for phantom
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+            <div className="button-container">
+                <WalletMultiButton className="cta-button connect-wallet-button" />
+            </div>
+        </div>
+    );
+
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">NFT drop machine with fair mint</p>
+                    {/* Render your connect to wallet button right here */}
+                    {renderNotConnectedContainer()}
                 </div>
                 <div className="footer-container">
-                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-                    <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+                    <img alt="Twitter Logo" className="twitter-logo" src={TWITTER_LOGO} />
+                    <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`@${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
         </div>
